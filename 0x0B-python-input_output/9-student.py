481b1bd25245897845eb251d186f816d1ea19c34
Code021-raelle <gabrielakinshola3@gmail.com>
2024-01-10 14:32:58 -0800

    py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module defines a class student"""
+
+
+class Student:
+    """Represent a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Gets a dictionary representation of the student"""
+        return self.__dict__
