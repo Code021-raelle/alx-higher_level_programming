489c23ff7aa5525b0bb41240a40e7a1eddd937f5
Gabriel Akinshola <gabrielakinshola3@gmail.com>
2024-02-14 11:55:46 +0100

    MySQL

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- This script prints the full description of the 'first_table' from the 'hbtn_0c_0' database.
 
-SELECT CONCAT('CREATE TABLE `', TABLE_NAME, '` (', GROUP_CONCAT(CONCAT('\n  `', COLUMN_NAME, '` ', DATA_TYPE, IFNULL(CONCAT('(', CHARACTER_MAXIMUM_LENGTH, ')'), ''), ' ', IS_NULLABLE, ' ', EXTRA)), '\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci') AS Create_Table
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
