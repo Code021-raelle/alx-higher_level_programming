a8f60db6d77cfe235d09f99c85327f569c43984e
Gabriel Akinshola <gabrielakinshola3@gmail.com>
2024-03-12 13:14:47 +0100

    javascript

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const ParentSquare = require('./5-square');
+
+class Square extends ParentSquare {
+    charPrint(c) {
+        if (c === undefined)
+            c = 'X';
+
+        for (let i = 0; i < this.height; i++) {
+            console.log(c.repeat(this.width));
+        }
+    }
+}
+
+module.exports = Square;
