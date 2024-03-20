71082db4a1810f3a8d3d4a5e3177a830789d24c8
Code021-raelle <gabrielakinshola3@gmail.com>
2024-03-20 14:58:24 +0100

    py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,30 +3,29 @@
 Script that lists all states with a name starting with N from the database
 htbn_0e_0_usa.
 """
-
-
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    # Connect to MySQL server
-    conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3])
 
-    # Create a cursor object
-    cur = conn.cursor()
+def main(user, password, db):
+    # Connect to the database
+    db = MySQLdb.connect(
+            host="localhost", port=3306, user=user, passwd=password, db=db)
+    cursor = db.cursor()
 
-    # Execute SQL query
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    # Execute the query to select all states with names starting "N"
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    # Fetch all rows and print them
-    for row in cur.fetchall():
+    # Fetch all the rows
+    results = cursor.fetchall()
+
+    # Print each row
+    for row in results:
         print(row)
 
-    # Close cursor and connection
-    cur.close()
-    conn.close()
+    # Close the database connection
+    db.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
