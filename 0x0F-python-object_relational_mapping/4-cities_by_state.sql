92e3701bf26398e888e7bc03cf768b4f81c2563e
Code021-raelle <gabrielakinshola3@gmail.com>
2024-03-21 01:20:42 -0700

    sql

--- 0x0F-python-object_relational_mapping/4-cities_by_state.sql
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.sql
@@ -18,4 +18,4 @@ CREATE TABLE IF NOT EXISTS cities (
 INSERT INTO cities (state_id, name) VALUES (1, "San Francisco"), (1, "San Jose"), (1, "Los Angeles"), (1, "Fremont"), (1, "Livermore");
 INSERT INTO cities (state_id, name) VALUES (2, "Page"), (2, "Phoenix");
 INSERT INTO cities (state_id, name) VALUES (3, "Dallas"), (3, "Houston"), (3, "Austin");
-INSERT INTO cities (state_id, name) (5, "Las Vegas"), (5, "Reno"), (5, "Henderson"), (5, "Carson City");
+INSERT INTO cities (state_id, name) VALUES (5, "Las Vegas"), (5, "Reno"), (5, "Henderson"), (5, "Carson City");
