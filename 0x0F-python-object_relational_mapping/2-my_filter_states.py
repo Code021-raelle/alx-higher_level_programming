6362438132c097d57ae4557795ff067f6df687df
Code021-raelle <gabrielakinshola3@gmail.com>
2024-03-21 12:08:48 +0100

    sql

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,6 +11,13 @@ if __name__ == "__main__":
     if len(sys.argv) != 5:
         print("Usage: {} username password database_name state_name".format(sys.argv[0]))
         sys.exit(1)
+
+    # Retrieve command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
     # Connect to the database
     db = MySQLdb.connect(
             host="localhost", port=3306,
