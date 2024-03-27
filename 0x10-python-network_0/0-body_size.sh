d4c3bf7113f49eb30b78ebe759d8771fda6ddb7a
Code021-raelle <gabrielakinshola3@gmail.com>
2024-03-27 14:35:18 +0100

    network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 # This script sends a request to a given URL and displays the size of the response body in bytes
-
-# Check if a URL is provided
-if [ -z "$1" ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-# Send a request to the URL and get the size of the response bosy in bytes
-response_size=$(curl -s -o /dev/null -w "%{size_download}" "$1")
-
-# Display the size
-echo $response_size
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 # This script sends a GET request to a given URL and displays the body of the response if the status code is 200
-
-# Check if a URL is provided
-if [ -z "$1" ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-# Send a GET request to the URL and check the status code
-status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-
-# If the status code is 200, display the body of the response
-if [ "$status_code" -eq 200 ]; then
-	curl -s "$1"
-else
-	echo "Request failed with status code: $status_code"
-fi
+curl -sL -w "%{http_code}" -o /dev/null "$1" | {
+	read -r status_code
+	if [[ $status_code -eq 200 ]]; then
+		curl -sL "$1"
+	fi
+}
