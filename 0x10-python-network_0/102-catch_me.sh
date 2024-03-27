115ae3b2f3b12457a4483643c416048e2d713ba2
Code021-raelle <gabrielakinshola3@gmail.com>
2024-03-27 17:01:08 +0100

    network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # This script sends a request to 0.0.0.0:5000/catch_me and displays the response
-
-# Send a request to the specified URL and output the response directly
-curl -sL -X PUT -H -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -s "http://0.0.0.0:5000/catch_me"
